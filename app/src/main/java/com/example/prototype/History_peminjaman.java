eb8358600290724f4328bae223bd9cff4232051c
bagus.oetomo <bagus.oetomo11@gmail.com>
2021-01-06 21:41:04 +0700

    ini android git

--- app/src/main/java/com/example/prototype/History_peminjaman.java
+++ app/src/main/java/com/example/prototype/History_peminjaman.java
@@ -1,8 +1,13 @@
 package com.example.prototype;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class History_peminjaman extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class History_peminjaman extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_peminjaman);
+
+        //Menampilkan halaman Fragment yang pertama kali muncul
+        getFragmentPage(new Fragment_history());
+
+        /*Inisialisasi BottomNavigationView beserta listenernya untuk
+         *menangkap setiap kejadian saat salah satu menu item diklik
+         */
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                //Menantukan halaman Fragment yang akan tampil
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragment = new Fragment_history();
+                        break;
+
+                    case R.id.profile:
+                        fragment = new Status_mobil();
+                        break;
+                }
+                return getFragmentPage(fragment);
+            }
+        });
+    }
+
+    //Menampilkan halaman Fragment
+    private boolean getFragmentPage(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.page_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
